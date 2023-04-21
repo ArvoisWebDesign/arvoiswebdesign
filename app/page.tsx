5707876a1a1efdbccdb9175767b9d1dc42185b7b
ArvoisWebDesign <contactdaera@gmail.com>
2023-04-21 20:43:03 +0200

    Fixed responsiveness of home page title

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
     return (
         <div className="text-center drop-shadow-aw-dark-8">
-            <h1 className="font-black text-6xl italic leading-6xl">ARVOIS WEBDESIGN</h1>
-            <h2 className="font-bold text-2xl text-muted-foreground">Vous avez dit portfolio?</h2>
+            <h1 className="font-black text-4xl md:text-5xl lg:text-6xl italic leading-8xl mb-8">ARVOIS WEBDESIGN</h1>
+            <h2 className="font-bold text-xl text-muted-foreground">Vous avez dit portfolio?</h2>
         </div>
     )
 }
